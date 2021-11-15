5cb4ec43df489789a741860244e91124ea911c45
Marlene Taca <marlene.taca@intive.com>
2021-11-15 11:53:29 -0300

    changes in layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Personalinfo from './Components/PersonalInfo/Personalinfo';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+	<BrowserRouter>
+		<div className="mainBackground">
+			<Routes>
+				<Route path='/' element={<Personalinfo/>}/>
+				<Route path='/PersonalInfo' element={<Personalinfo/>}/>
+				<Route path='*' element={<Personalinfo/>}/>
+			</Routes>
+		</div>
+	</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/Components/PersonalInfo/Personalinfo.jsx
@@ -0,0 +1,11 @@
+import './Personalinfo.css';
+
+const Personalinfo = () => {
+    return (
+        <div className="personalInfo">
+
+        </div>
+    );
+}
+
+export default Personalinfo;
