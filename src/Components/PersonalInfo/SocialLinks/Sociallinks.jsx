3fff7a482b9fcffbe989e205469e6a4f6e52e800
Marlene Taca <marlene.taca@intive.com>
2021-11-24 09:44:59 -0300

    github added

--- src/Components/PersonalInfo/SocialLinks/Sociallinks.jsx
+++ src/Components/PersonalInfo/SocialLinks/Sociallinks.jsx
@@ -3,7 +3,10 @@ import './Sociallinks.css';
 const Sociallinks = () => {
     return(
         <div className="socialLinks">
-            <p>Find me in <a href="https://www.linkedin.com/in/marlene-taca/"><i className="fab fa-linkedin"></i></a></p>
+            <p>Find me in 
+                <a href="https://www.linkedin.com/in/marlene-taca/"><i className="fab fa-linkedin"></i></a>
+                <a href="https://github.com/AlessaMarlene"><i class="fab fa-github"></i></a>
+            </p>
         </div>
     );
 }
